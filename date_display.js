39f55b63df22567e1e43094312a284a7e6144502
Tristan <43931769+loli-ryona@users.noreply.github.com>
2018-12-18 16:12:29 +1100

    Add files via upload

---
+++ date_display.js
@@ -0,0 +1,28 @@
+//Date Display by Tristan, ManageIT Services
+function fourDigitYear(number)	{						//creates a function to correctly calculate the year
+	return (number < 1000) ? number + 1900 : number;	
+}
+
+today =  days[now.getDay()] + ", " +					//creates the date and time display at displays it on the html document
+         months[now.getMonth()] + " " +
+         date + ", " +
+         (fourDigitYear(now.getYear())) ;
+	document.write(today);
+	
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+	function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+	
